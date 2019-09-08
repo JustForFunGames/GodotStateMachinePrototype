7252d70019c776796b58ab110cc2c97ea3a03647
Micha Grandel <michagrandel@googlemail.com>
2019-09-08 12:36:15 +0200

    add theme for font settings
    Add a theme with font settings to manage font settings
    for the labels in one place.

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -2,7 +2,20 @@ extends Node2D
 
 var version = "Prototype A"
 
+func _ready():
+	# todo: replace the node selection with something more fun
+	$"UI/Prototype/VBoxContainer/HBoxContainer/VersionLabel".text = version
+	$"UI/Prototype/VBoxContainer/HBoxContainer/DebugLabel".text = ""
+	$"UI/Prototype/VBoxContainer/HBoxContainer/TimerLabel".text = ""
+
 func _on_change_state(state):
-	var label_text = "Version: %s; State: %s"
-	$"UserInterfaces/PrototypeUI/VBoxContainer/Label".text = \
-		label_text % [version, state]
+	var text = "State: %s"
+	$"UserInterfaces/PrototypeUI/VBoxContainer/HBoxContainer/Debug".text = text % state
+
+func _on_change_timer(seconds):
+	var text = ""
+	
+	if seconds > 1:
+		text = str(seconds)
+	
+	$"UserInterfaces/PrototypeUI/VBoxContainer/HBoxContainer/Debug".text = text
